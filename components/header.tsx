9db471592088b68a6fdb22c29c0b403b06eb1b05
Chad - ubuntu <scderya@gmail.com>
2020-11-27 19:46:22 +0000

    Making it more Agile Import

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,19 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 const Header = () => {
   return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
       <Link href="/">
-        <a className="hover:underline">Blog</a>
+        <a>
+          <div className="font-bold tracking-tighter leading-tight md:pr-8">
+            <span className=" bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500">
+              Agile import
+            </span>
+          </div>
+        </a>
       </Link>
-      .
     </h2>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- components/intro.tsx
+++ components/intro.tsx
@@ -1,23 +1,17 @@
-import { CMS_NAME } from '../lib/constants'
 
 const Intro = () => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
-      </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{' '}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-          Next.js
-        </a>{' '}
-        and {CMS_NAME}.
+      <div className="text-4xl sm:text-6xl md:text-6xl la:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+        <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500">
+          Agile import
+        </span>
+      </div>
+      <h4 className="text-center md:text-left text-lg la:text-lg mt-5 md:pl-4">
+        Customs Declaration System for UK traders to use after BREXIT.
       </h4>
     </section>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
