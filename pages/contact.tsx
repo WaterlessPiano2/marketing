1e581bb103cc415b12ca831429dc0663c818b468
Chad - ubuntu <scderya@gmail.com>
2020-12-27 15:31:54 +0000

    contact us works and looks better

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -10,9 +10,9 @@ import emailjs from "emailjs-com";
 const About = () => {
   const router = useRouter(),
     pageName = getPageName(router.pathname),
-    serviceID = process.env.SERVICE_ID || "",
-    templateID = process.env.TEMPLATE_ID || "",
-    userID = process.env.USER_ID || "";
+    serviceID = "service_y1oyp8p",
+    templateID = "template_000xae9",
+    userID = "user_X9a7OgOOTYJqqTpKOtifY";
 
   const sendEmail = (e: any) => {
     e.preventDefault();
@@ -40,17 +40,52 @@ const About = () => {
               <div className="lg:text-center">
                 <Intro pageName={pageName} />
               </div>
-              <div className="lg:text-center">
-                <form className="contact-form" onSubmit={sendEmail}>
-                  <label>Name</label>
-                  <input type="text" name="name" />
-                  <label>Email</label>
-                  <input type="email" name="email" />
-                  <label>Message</label>
-                  <textarea name="message" />
-                  <input type="submit" value="Send" />
-                </form>
-              </div>
+              <p className=" mt-20 text-center">
+                If you would like a demo, or make a suggestion just let us know
+                by filling the form below. We will get back to you within 24
+                hours.
+              </p>
+              <form
+                className="mt-20 contact-form grid grid-cols-1 grid-rows-3 gap-4"
+                onSubmit={sendEmail}
+              >
+                {" "}
+                <div className="py-2 grid grid-cols-2 ">
+                  <label className="block-inline font-medium text-gray-700">
+                    Name:
+                  </label>
+                  <input
+                    type="text"
+                    name="name"
+                    className="max-h-8 p-2 block-inline sm:text-sm  border border-gray-500 rounded-md "
+                  />
+                </div>{" "}
+                <div className="py-2 grid grid-cols-2">
+                  <label className="block-inline font-medium text-gray-700">
+                    Email:
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    className="max-h-8 p-2 block-inline sm:text-sm border border-gray-500 rounded-md "
+                  />
+                </div>{" "}
+                <div className="py-2 grid grid-cols-2">
+                  <label className="block-inline font-medium text-gray-700">
+                    Messsage:
+                  </label>
+                  <textarea
+                  rows={4}
+                    name="message"
+                    className="p-2 block-inline sm:text-sm border border-gray-500 rounded-md "
+                  />
+                </div>
+                <input
+                  className=" rounded w-1/5 bg-blue-400 "
+                  type="submit"
+                  value="Send"
+                />
+              </form>
             </div>
           </div>
         </Container>
